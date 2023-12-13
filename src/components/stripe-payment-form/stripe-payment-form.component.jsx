fbb480301c448a5e1e588f487e0ef6466f415d3a
l-v-react <109346622+laszlo-v@users.noreply.github.com>
2023-12-13 09:10:16 +0000

    redirect test

--- src/components/stripe-payment-form/stripe-payment-form.component.jsx
+++ src/components/stripe-payment-form/stripe-payment-form.component.jsx
@@ -38,13 +38,13 @@ const StripePaymentForm = ({ totalPrice }) => {
         },
       },
     });
-    if (paymentResult.error) {
-      alert(paymentResult.error);
-    } else {
-      if (paymentResult.paymentIntent.status === "succeeded") {
-        navigate("/", { replace: true });
-      }
-    }
+    navigate("/", { replace: true });
+    // if (paymentResult.error) {
+    //   alert(paymentResult.error);
+    // } else {
+    //   if (paymentResult.paymentIntent.status === "succeeded") {
+    //   }
+    // }
   };
 
   return (
